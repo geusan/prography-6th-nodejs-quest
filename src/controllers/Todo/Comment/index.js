cb785de4723e2cab9c6c141a6213735b4d05dd31
geusan <dnay2k@gmail.com>
2020-02-17 01:48:22 +0900

    Modify logic

--- src/controllers/Todo/Comment/index.js
+++ src/controllers/Todo/Comment/index.js
@@ -8,36 +8,28 @@ router.post('/', (req, res) => {
   comment.set(Comment.CONTENTS, req.body.contents);
   comment.set(Comment.TODO_ID, req.params.todoId);
   comment.save();
-  res.send({
-    data: comment
-  })
+  res.send(comment)
 });
 router.get('/', (req, res) => {
   const todo = Todo.find(req.params.todoId);
   const comments = todo.getComments();
-  res.send({
-    data: comments
-  })
+  res.send(comments)
 });
 router.get('/:commentId', (req, res) => {
   const comment = Comment.find(req.params.commentId);
-  res.send({
-    data: comment
-  })
+  res.send(comment)
 });
 router.put('/:commentId', (req, res) => {
   const comment = Comment.find(req.params.commentId);
   comment.set(Comment.CONTENTS, req.body.contents || comment.get(Comment.CONTENTS));
   comment.save();
-  res.send({
-    data: comment
-  })
+  res.send(comment)
 });
 router.delete('/:commentId', (req, res) => {
   const comment = Comment.find(req.params.commentId);
   comment.remove();
   res.send({
-    data: comment
+    msg: 'success',
   })
 });
 
